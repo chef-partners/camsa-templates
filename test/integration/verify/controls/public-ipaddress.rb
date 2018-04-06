246a6425f07ea5a98bc84ff1bed4d10a05747fad
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2018-04-06 16:22:00 +0100

    Added virtual network template and modified main template Updated tests with attribute values
    Signed-off-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- test/integration/verify/controls/public-ipaddress.rb
+++ test/integration/verify/controls/public-ipaddress.rb
@@ -1,15 +1,18 @@
+resource_group_name = attribute('resource_group_name', default: 'InSpec-AMA', description: 'Name of the resource group to interogate')
+unique_string = attribute('unique_string', default: '9j2f')
+
 title 'Check Public IP Addresses'
 
 control 'Automate-Server-Public-IP-Address' do
   impact 1.0
   title 'Ensure that the settings are correct for the public IP address for the Automate server'
 
-  describe azure_generic_resource(group_name: 'ama-20180406-6', name: 'inspec-automate-9j2f-PublicIP') do
+  describe azure_generic_resource(group_name: resource_group_name, name: "inspec-automate-#{unique_string}-PublicIP") do
     its('location') { should cmp 'westeurope' }
     its('properties.provisioningState') { should cmp 'Succeeded' }
     its('properties.publicIPAddressVersion') { should cmp 'IPv4' }
     its('properties.publicIPAllcationMethod') { should cmp 'Dynamic' }
-    its('properties.dnsSettings.domainNameLabel') { should cmp 'inspec-automate-9j2f' }
+    its('properties.dnsSettings.domainNameLabel') { should cmp "inspec-automate-#{unique_string}" }
   end
 end
 
@@ -17,11 +20,11 @@ control 'Chef-Server-Public-IP-Address' do
   impact 1.0
   title 'Ensure that the settings are correct for the public IP address for the Chef server'
 
-  describe azure_generic_resource(group_name: 'ama-20180406-6', name: 'inspec-chef-9j2f-PublicIP') do
+  describe azure_generic_resource(group_name: resource_group_name, name: "inspec-chef-#{unique_string}-PublicIP") do
     its('location') { should cmp 'westeurope' }
     its('properties.provisioningState') { should cmp 'Succeeded' }
     its('properties.publicIPAddressVersion') { should cmp 'IPv4' }
     its('properties.publicIPAllcationMethod') { should cmp 'Dynamic' }
-    its('properties.dnsSettings.domainNameLabel') { should cmp 'inspec-chef-9j2f' }
+    its('properties.dnsSettings.domainNameLabel') { should cmp "inspec-chef-#{unique_string}" }
   end
 end
--- test/integration/verify/controls/resources.rb
+++ test/integration/verify/controls/resources.rb
@@ -1,14 +1,23 @@
+resource_group_name = attribute('resource_group_name', default: 'InSpec-AMA', description: 'Name of the resource group to interogate')
+unique_string = attribute('unique_string', default: '9j2f')
+
 title 'Check all resources are present'
 
 control 'Azure-Managed-Automate-Resources' do
   impact 1.0
   title 'Determine that all resources for the Managed Application have been created'
 
-  describe azure_generic_resource(group_name: 'ama-20180406-6') do
+  describe azure_generic_resource(group_name: resource_group_name) do
+    # There should be at least 1 virtual network which should be the one that we created
+    # for support purposes. Although it is recommended that the managed app is deployed into
+    # an isolated resource group there is nothing to enforce this so there maybe more than one
+    # virtual network in the resource group
+    its('Microsoft.Network/virtualNetworks') { should >= 1 }
+
     # It should have two public IP addresses
     its('Microsoft.Network/publicIPAddresses') { should eq 2 }
 
     # It should have two network interfaces, one for each vm
-    its('Microsoft.Network/networkInterfaces') { should eq 2 }
+    its('Microsoft.Network/networkInterfaces') { should eq 4 }
   end
 end
