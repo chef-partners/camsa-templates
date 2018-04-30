440eb8da089292a7b14c50c005c41031938d94ee
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2018-04-30 14:06:05 +0100

    Added attributes file to be patched by the build process Updated test.sh script
    Signed-off-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- bin/tests.sh
+++ bin/tests.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-inspec exec test/integration/verify -t azure:// --reporter junit:inspec.out
+inspec exec test/integration/verify -t azure:// --reporter junit:inspec.out --attrs test/integration/inspec-attrs.yaml
