21615fac8768bdcb5a66bc4c5762d3a82a6d283e
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2018-12-03 13:29:12 +0000

    Added typescript liniting Tidied up Typscript files
    Signed-off-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- lib/Utils.ts
+++ lib/Utils.ts
@@ -1,13 +1,13 @@
 export class Utils {
-  public static isNotEmpty(str: string): boolean {
-      return (!!str && !!str.trim())
-  }
+    public static isNotEmpty(str: string): boolean {
+        return (!!str && !!str.trim());
+    }
 
-  public static getError(error: any): string {
-      if (error && error.message) {
-          return JSON.stringify(error.message)
-      }
+    public static getError(error: any): string {
+        if (error && error.message) {
+            return JSON.stringify(error.message);
+        }
 
-      return JSON.stringify(error)
-  }
+        return JSON.stringify(error);
+    }
 }
